a5d0f25cdea4c17da43a6f8a23c7225acb70c277
d0vgan <dvv81@ukr.net>
2021-04-18 19:00:16 +0300

    Enhancing Build_VC2017_*.cmd

--- Source/Build_VC2017_win32.cmd
+++ Source/Build_VC2017_win32.cmd
@@ -1,17 +1,38 @@
-call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
+@echo off
+
+set VC_ROOT=%ProgramFiles(x86)%\Microsoft Visual Studio\2017
+
+if exist "%VC_ROOT%\Professional\VC\Auxiliary\Build\vcvarsall.bat" goto UseVcProfessional
+if exist "%VC_ROOT%\Community\VC\Auxiliary\Build\vcvarsall.bat" goto UseVcCommunity
+
+goto ErrorNoVcVarsAll
+
+:UseVcProfessional
+call "%VC_ROOT%\Professional\VC\Auxiliary\Build\vcvarsall.bat" x86
+goto Building
+
+:UseVcCommunity
+call "%VC_ROOT%\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
+goto Building
+
+:Building
 msbuild XBrackets_vc2017.sln /t:Clean /p:Configuration=Release;Platform=Win32
 msbuild XBrackets_vc2017.sln /t:Build /p:Configuration=Release;Platform=Win32
 
-@echo off
-
 if exist .\Release\XBrackets.dll goto CopyDll
 echo.
-echo FAILED: XBrackets.dll has not been built!
+echo Build FAILED: XBrackets.dll has not been built!
 goto End
 
 :CopyDll
 echo.
-echo OK: Copying XBrackets.dll to ..\Plugs
-copy .\Release\XBrackets.dll ..\Plugs /Y
+echo Build OK: Copying XBrackets.dll to ..\Plugs
+if not exist ..\Plugs\ mkdir ..\Plugs
+copy .\Release\XBrackets.dll ..\Plugs\ /Y
+goto End
+
+:ErrorNoVcVarsAll
+echo ERROR: Could not find "vcvarsall.bat"
+goto End
 
 :End
--- Source/Build_VC2017_x64.cmd
+++ Source/Build_VC2017_x64.cmd
@@ -1,17 +1,38 @@
-call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+@echo off
+
+set VC_ROOT=%ProgramFiles(x86)%\Microsoft Visual Studio\2017
+
+if exist "%VC_ROOT%\Professional\VC\Auxiliary\Build\vcvarsall.bat" goto UseVcProfessional
+if exist "%VC_ROOT%\Community\VC\Auxiliary\Build\vcvarsall.bat" goto UseVcCommunity
+
+goto ErrorNoVcVarsAll
+
+:UseVcProfessional
+call "%VC_ROOT%\Professional\VC\Auxiliary\Build\vcvarsall.bat" x64
+goto Building
+
+:UseVcCommunity
+call "%VC_ROOT%\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+goto Building
+
+:Building
 msbuild XBrackets_vc2017.sln /t:Clean /p:Configuration=Release;Platform=x64
 msbuild XBrackets_vc2017.sln /t:Build /p:Configuration=Release;Platform=x64
 
-@echo off
-
 if exist .\x64\Release\XBrackets.dll goto CopyDll
 echo.
-echo FAILED: XBrackets.dll has not been built!
+echo Build FAILED: XBrackets.dll has not been built!
 goto End
 
 :CopyDll
 echo.
-echo OK: Copying XBrackets.dll to ..\Plugs64
-copy .\x64\Release\XBrackets.dll ..\Plugs64 /Y
+echo Build OK: Copying XBrackets.dll to ..\Plugs64
+if not exist ..\Plugs64\ mkdir ..\Plugs64
+copy .\x64\Release\XBrackets.dll ..\Plugs64\ /Y
+goto End
+
+:ErrorNoVcVarsAll
+echo ERROR: Could not find "vcvarsall.bat"
+goto End
 
 :End
