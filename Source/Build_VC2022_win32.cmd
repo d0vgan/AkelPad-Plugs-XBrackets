6760825b9caae7ab54b4021c6f17704fecee3c3e
d0vgan <dvv81@ukr.net>
2023-06-04 23:17:16 +0300

    adding VC2022 project

---
+++ Source/Build_VC2022_win32.cmd
@@ -0,0 +1,38 @@
+@echo off
+
+set VC_ROOT=%ProgramFiles%\Microsoft Visual Studio\2022
+
+if exist "%VC_ROOT%\Professional\VC\Auxiliary\Build\vcvarsall.bat" goto UseVcProfessional
+if exist "%VC_ROOT%\Community\VC\Auxiliary\Build\vcvarsall.bat" goto UseVcCommunity
+
+goto ErrorNoVcVarsAll
+
+:UseVcProfessional
+call "%VC_ROOT%\Professional\VC\Auxiliary\Build\vcvarsall.bat" x86
+goto Building
+
+:UseVcCommunity
+call "%VC_ROOT%\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
+goto Building
+
+:Building
+msbuild XBrackets_vc2022.sln /t:Clean /p:Configuration=Release;Platform=Win32
+msbuild XBrackets_vc2022.sln /t:Build /p:Configuration=Release;Platform=Win32
+
+if exist .\Release\XBrackets.dll goto CopyDll
+echo.
+echo Build FAILED: XBrackets.dll has not been built!
+goto End
+
+:CopyDll
+echo.
+echo Build OK: Copying XBrackets.dll to ..\Plugs
+if not exist ..\Plugs\ mkdir ..\Plugs
+copy .\Release\XBrackets.dll ..\Plugs\ /Y
+goto End
+
+:ErrorNoVcVarsAll
+echo ERROR: Could not find "vcvarsall.bat"
+goto End
+
+:End
---
+++ Source/Build_VC2022_x64.cmd
@@ -0,0 +1,38 @@
+@echo off
+
+set VC_ROOT=%ProgramFiles%\Microsoft Visual Studio\2022
+
+if exist "%VC_ROOT%\Professional\VC\Auxiliary\Build\vcvarsall.bat" goto UseVcProfessional
+if exist "%VC_ROOT%\Community\VC\Auxiliary\Build\vcvarsall.bat" goto UseVcCommunity
+
+goto ErrorNoVcVarsAll
+
+:UseVcProfessional
+call "%VC_ROOT%\Professional\VC\Auxiliary\Build\vcvarsall.bat" x64
+goto Building
+
+:UseVcCommunity
+call "%VC_ROOT%\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+goto Building
+
+:Building
+msbuild XBrackets_vc2022.sln /t:Clean /p:Configuration=Release;Platform=x64
+msbuild XBrackets_vc2022.sln /t:Build /p:Configuration=Release;Platform=x64
+
+if exist .\x64\Release\XBrackets.dll goto CopyDll
+echo.
+echo Build FAILED: XBrackets.dll has not been built!
+goto End
+
+:CopyDll
+echo.
+echo Build OK: Copying XBrackets.dll to ..\Plugs64
+if not exist ..\Plugs64\ mkdir ..\Plugs64
+copy .\x64\Release\XBrackets.dll ..\Plugs64\ /Y
+goto End
+
+:ErrorNoVcVarsAll
+echo ERROR: Could not find "vcvarsall.bat"
+goto End
+
+:End
